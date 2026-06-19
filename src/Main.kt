971c9c3a5bf169644ac847757ce305f237a0517e
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2026-06-19 02:55:32 -0500

    add: seguridad del pipelin

--- src/Main.kt
+++ src/Main.kt
@@ -6,16 +6,29 @@ import java.io.File
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args : Array<String>) {
 
-    val raiz = verificarEntrada(args)
+    val args = verificarEntrada(args)
 
-    val arbol : Arbol = Arbol(raiz)
-    arbol.crearSubdirectorios()
-    print(arbol.nPalabraMasLarga())
 
 }
 
 fun verificarEntrada(args : Array<String>) : Array<String> {
     if(args.isEmpty())
-        throw IllegalArgumentException("[Main] Los argumentos de entrada están incompletos")
+        throw IllegalArgumentException("[Main] Los argumentos de entrada están vacíos mani")
+
+    if(args.size > 1)
+        throw IllegalArgumentException("[Main] Se espera un solo argumento en la terminal")
+
     return args
+}
+
+fun generarPath(args : Array<String>) : File {
+    val path = File(args.first())
+
+    if(!path.exists())
+        throw IllegalArgumentException("[Main] El path no existe cabrón")
+
+    if(path.isFile)
+        throw IllegalArgumentException("[Main] El path es un archivo, no un directorio")
+
+    return path
 }
