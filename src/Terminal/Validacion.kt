ce3bb0f27049d89b442f31311be4344edd12beb7
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2026-07-05 22:40:16 -0500

    refactor: funciones de validacion movidas a su respectiva carpeta

--- src/Terminal/Validacion.kt
+++
@@ -1,4 +0,0 @@
-package src.Terminal
-
-class Validacion {
-}
---
+++ src/cli/Validacion.kt
@@ -0,0 +1,209 @@
+package src.Terminal
+
+import java.io.File
+import kotlin.system.exitProcess
+
+class Validacion() {
+
+    //pone los flags según lo que digan
+    fun validarFlags(args : Array<String>) : Pair<Map<String, Any?>, ArrayList<String>> {
+
+        val opcionesValidas = listOf(
+            //Caso base explorar/ver arbol
+            "--simple","--ocultos", "--reversar", "--recortar",
+            "--prueba", "--nivelMax", "--README", "--toArchivo",
+            "--ayuda", "--descripcion",
+
+            //Caso búsquedas
+            "--condicion", "--force"
+        )
+
+        val nuevosArgs : ArrayList<String> = arrayListOf()
+        val argsRestar : ArrayList<String> = arrayListOf()
+
+        val diccionarioFlags = mutableMapOf<String, Any?>(
+            "simple" to false,
+            "caracterEspacio" to " ",
+            "nRepeticiones" to 3,
+            "ocultos" to false,
+            "reversar" to false,
+            "recortar" to null,
+            "prueba" to false,
+            "nivelMax" to null,
+            "README" to false,
+            "toArchivo" to null,
+            "descripcion" to false,
+            "condicion" to null,
+            "force" to false
+        )
+
+        for((indice, argumento) in args.withIndex()){
+
+            if(argumento !in opcionesValidas) {
+                nuevosArgs.add(argumento)
+                continue
+            }
+
+            when(argumento){
+                "--ocultos" -> diccionarioFlags["ocultos"] = true
+                "--reversar" -> diccionarioFlags["reversar"] = true
+                "--prueba" -> diccionarioFlags["prueba"] = true
+                "--force" -> diccionarioFlags["force"] = true
+                "--simple" -> {
+                    val siguiente = args.getOrNull(indice + 1)
+
+                    if(siguiente != null && siguiente.length == 1){
+                        argsRestar.add(siguiente)
+                        diccionarioFlags["caracterEspacio"] = siguiente
+
+                        val siguientesiguiente = args.getOrNull(indice + 2)?.toIntOrNull()
+
+                        if(siguientesiguiente != null){
+                            argsRestar.add(siguientesiguiente.toString())
+                            diccionarioFlags["nRepeticiones"] = siguientesiguiente
+                        }
+                    }
+
+                    diccionarioFlags["simple"] = true
+                }
+                "--condicion" -> {
+                    val siguiente = args.getOrNull(indice + 1)
+
+                    if(siguiente == null){
+                        System.err.println("[Main] Después de --condicion no había palabra")
+                        exitProcess(1)
+                    }
+
+                    diccionarioFlags["condicion"] = siguiente
+                    argsRestar.add(siguiente)
+                }
+                "--README" -> {
+                    val siguiente = args.getOrNull(indice + 1)
+                    diccionarioFlags["README"] = true
+
+                    if(siguiente == "desc") {
+                        diccionarioFlags["descripcion"] = true
+                        argsRestar.add(siguiente)
+                    }
+
+                }
+                "--recortar" -> {
+                    val siguiente = args.getOrNull(indice + 1)?.toIntOrNull()
+                    val siguienteEliminar = args.getOrNull(indice + 1)  //este es para eliminar el numero de los args
+                    //para que no de problemas de cantidad de args y se meta en
+                    //otras opciones
+
+                    if(siguiente == null || siguienteEliminar == null) {
+                        System.err.println("[Main] Después de --recortar no había número")
+                        exitProcess(1)
+                    }
+
+                    diccionarioFlags["recortar"] = siguiente
+                    argsRestar.add(siguienteEliminar)
+                }
+                "--nivelMax" -> {
+                    val siguiente = args.getOrNull(indice + 1)?.toIntOrNull()
+                    val siguienteEliminar = args.getOrNull(indice + 1)  //este es para eliminar el numero de los args
+                    //para que no de problemas de cantidad de args y se meta en
+                    //otras opciones
+
+                    if(siguiente == null || siguienteEliminar == null) {
+                        System.err.println("[Main] después de --nivelMax no había número")
+                        exitProcess(1)
+                    }
+
+                    diccionarioFlags["nivelMax"] = siguiente
+                    argsRestar.add(siguienteEliminar)
+                }
+                "--toArchivo" -> {
+                    val siguiente = args.getOrNull(indice + 1)
+                    val siguienteEliminar = args.getOrNull(indice + 1)  //este es para eliminar el numero de los args
+                    //para que no de problemas de cantidad de args y se meta en
+                    //otras opciones
+
+                    if(siguiente == null || siguienteEliminar == null) {
+                        System.err.println("[Main] Hubo un error al leer el archivo después de --toArchivo")
+                        exitProcess(1)
+                    }
+
+                    val path = File(siguiente)
+                    if(!path.exists() && !path.isFile){
+                        System.err.println("[Main] el path a escribir no existe o no es un archivo")
+                        exitProcess(1)
+                    }
+
+                    diccionarioFlags["toArchivo"] = path
+                    argsRestar.add(siguiente)
+                }
+                "--ayuda" -> {
+                    println(
+                        """
+                    
+                    flags: (entre [] caracteres opcionales) 
+                    --simple [caracter] [n]    //imprime el arbol sin caracteres especiales, puedes colocar el char que quieras y que tanto se repiten
+                    --ocultos                  //mostrar carpetas y archivos ocultos
+                    --reversar                 //reversar las listas internas y mostrar primero archivos que directorios
+                    --recortar n               //recorta palabras hasta ese tamaño (falta añadir compatibilidad con --simple usar regex distintos)
+                    --prueba                   //lanzar prueba
+                    --nivelMax n               //hacerlo hasta el nivel max (0 es nada, 1 es la raiz, 2 el segundo nivel ...)
+                    --README [desc]            //crear README (desc es opcional para activar la descripcion) (mejorar integracion con readmes ya existentes)
+                    --toArchivo "path_archivo" //guardar en archivo (borra el contenido)
+                    --ayuda                    //Imprime este txt
+                    
+                    comandos:
+                    ...main.jar "path_original" //genera un arbol
+                    ...main.jar "path_original" "borrar" "palabra" 				            //elimina esa palabra de todo el arbol (falta implementar)
+                    ...main.jar "path_original" "buscar" "palabra" --condicion "condicion" 	//busca archivo que contenga esa palabra
+                    						        		                                //lo busca exacto o conteniendo, tiene que tener un flag, o por default
+                    									                                    // sale exacto
+                    ...main.jar "path_original" "reemplazar" "palabraVieja" "palabraNueva"  //reemplaza palabra dentro de cada archivo
+                    
+                    compilar:
+                    kotlinc *.kt -include-runtime -d main.jar
+                """.trimIndent()
+                    )
+                    exitProcess(1)
+                }
+            }
+
+
+        }
+
+        val iniciarRestar = 0
+        var iterador = 0
+        while (iterador <= nuevosArgs.size){
+            if(argsRestar.isEmpty())
+                break
+
+            if(nuevosArgs[iterador] == argsRestar[iniciarRestar]){
+                nuevosArgs.removeAt(iterador)
+                argsRestar.removeAt(iniciarRestar)
+            }
+            else
+                iterador++
+        }
+
+        return Pair(diccionarioFlags, nuevosArgs)
+    }
+
+    //según la linea de comandos que entre, revisar qué caso es y asignar un número de función
+    fun verificarEntrada(args: ArrayList<String>) : Int{
+        if(args.isEmpty()){
+            System.err.println("[Main] los argumentos están vacíos.")
+            exitProcess(1)
+        }
+
+        return when{
+            //un argumento, simplemente el arbol
+            args.size == 1 -> 1
+            args.size == 2 -> 2
+            (args.size == 3) && (args[1] == "borrar") -> 3
+            (args.size == 3) && (args[1] == "buscar") -> 4
+            (args.size == 4) && (args[1] == "reemplazar") -> 5
+            else -> {
+                System.err.println("[Main] Los argumentos recibidos no sirven, inserta --ayuda para una guía")
+                exitProcess(1)
+            }
+        }
+    }
+}
