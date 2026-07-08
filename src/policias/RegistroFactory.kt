63519c3903b6230a9f7cdb1e4601e692411eae81
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2026-07-08 09:11:42 -0500

    create: factories

---
+++ src/policias/RegistroFactory.kt
@@ -0,0 +1,7 @@
+package src.policias
+
+object RegistroFactory {
+}
+
+//hacer un factory de dataclasses y se pasa por constructor del policiafactory, así cada policia tiene un factory de dataclasses
+//el policia se asigna en el arbol
