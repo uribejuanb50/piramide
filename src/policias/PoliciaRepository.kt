92de752df9f15bf39186160d805f26849ef81758
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2026-07-09 10:57:28 -0500

    add: ahora se escriben los policias de categoria arbol, también se pueden leer (hablando de objetos)

---
+++ src/policias/PoliciaRepository.kt
@@ -0,0 +1,34 @@
+package src.policias
+
+import com.google.gson.Gson
+import src.utils.Rutas
+import java.nio.file.Files
+import java.nio.file.Path
+
+class PoliciaRepository(
+    val rutaGuardadoArbol : Path = Rutas.carpetaPolicias.resolve("PoliciaArbol.json")
+) {
+
+    private val gson = Gson()
+
+    fun cargarPoliciaArbol() : Policia?{
+        if(!Files.exists(rutaGuardadoArbol)) return null
+        val json = Files.readString(rutaGuardadoArbol)
+        return gson.fromJson(json, PoliciaArbol::class.java)
+    }
+
+    fun guardarPoliciaArbol(policiaArbol: PoliciaArbol) {
+        Files.createDirectory(rutaGuardadoArbol.parent)
+        val json = gson.toJson(policiaArbol)
+        Files.writeString(rutaGuardadoArbol, json)
+    }
+    fun cargarListaPoliciaBorrar(){
+
+    }
+    fun cargarListaPoliciaReemplazar(){
+
+    }
+    fun cargarListaPoliciaAplazar(){
+
+    }
+}
