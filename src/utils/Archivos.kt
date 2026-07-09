88277f303ae888296d87036f4c63ac46e1ac9f1f
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2026-07-09 10:45:24 -0500

    add: búsqueda de la raíz del proyecto, interoperativo entre dispositivos

--- src/utils/Archivos.kt
+++ src/utils/Archivos.kt
@@ -1,6 +1,9 @@
 package src.utils
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 fun escribirArchivo(contenido : String, path : File) : String{
     try{
@@ -9,4 +12,27 @@ fun escribirArchivo(contenido : String, path : File) : String{
         return "No se pudo escribir en el archivo. \n$e"
     }
     return "Contenido escrito exitosamente."
+}
+
+object Rutas{
+    val raizProyecto : Path by lazy{
+        val location = Rutas::class.java.protectionDomain.codeSource.location.toURI()
+        var actual = Paths.get(location).toAbsolutePath()
+
+        if(Files.isRegularFile(actual))
+            actual = actual.parent
+
+        else{
+            while(actual != null && !Files.exists(actual.resolve("data")))
+                actual = actual.parent
+        }
+
+        actual ?: throw IllegalStateException("[Rutas] No se pudo determinar la raíz del proyecto")
+    }
+
+    val carpetaPolicias : Path by lazy {
+        raizProyecto.resolve("data").resolve("").also{
+            Files.createDirectory(it)
+        }
+    }
 }
